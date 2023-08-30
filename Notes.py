819f4866767eab73469f97f8ef8d1392636d5056
quantumecho1 <143340219+quantumecho1@users.noreply.github.com>
2023-08-30 19:48:04 +0200

    Update Notes.py

--- Notes.py
+++ Notes.py
@@ -1,51 +1,37 @@
-# Funktion, die zehn Zeilenumbrüche ausgibt
 def F1():
     print('\n'*10)
 F1()
 
-# Erstellung und Kombination von Vorname und Nachname
 sOne="K"
 sTwo="G"
 sThree=sOne+" "+sTwo
 
-# Ausgabe des vollen Namens in verschiedenen Formaten
 print(sThree)
 print(sTwo+",\t\n",sOne)
 print(sTwo+"\t"+sOne)
 print()
 
-# Ausgabe eines TExts
 print("Asayshi!")
 
-# Klarheit, Länge und PEP8 verletzt (CamelCase und kurze Namen)
 zOne="NMM"
-
-# Demonstration der Verwendung von String-Manipulation (Großbuchstaben)
 print(zOne)
 print(zOne.upper())
 print()
 
-# Demonstration der Verwendung von String-Manipulation (Zentrierung)
 zTwo="Cx"
-
-# Demonstration der Verwendung von Escape-Sequenzen und tab size
 print(zTwo)
 print(zTwo.center(35,"-"))
 
-# Demonstration der Verwendung von String-Slicing
 Str_="h\tw"
 print(Str_)
 print(Str_.expandtabs(tabsize=15))
 
-# Demonstration der Verwendung von Raw-Strings
 print(zTwo[0:1])
 print(zTwo[0:2])
 print(zTwo[0:3])
 print(zTwo[0:4])
 
-# Demonstration der Verwendung von Raw-Strings
 ti='c:\\trash'
 print(ti)
 
-# Beispiel für eine einfache Zuweisung einer Ganzzahl
 x1=2
